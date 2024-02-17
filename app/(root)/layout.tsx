6c31ac1175160943238249f3e0dfc18d14e531fd
Lukas-Santo-Puglisi <lsanpuglisi@gmail.com>
2024-02-17 14:39:16 +0100

    authentification implemented

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 
 const Layout = ( {children} : {children: React.ReactNode} ) => {
   return (
-    <main className='root' >
+    <main className="root" >
 
-        <div className='root-container'>
-            <div className='wrapper'>
-                {children}
-            </div>
+        
+      <div className="root-container">
+        <div className="wrapper">
+          {children}
         </div>
+      </div>
     </main>
   )}
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,19 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
-import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
+
+import "./globals.css";
 
 const IBMPlex = IBM_Plex_Sans({ 
   subsets: ["latin"],
-  weight: ['400', '500', '600',  '700'],
+  weight: ['400', '500', '600', '700'],
   variable: '--font-ibm-plex'
- });
+});
 
 export const metadata: Metadata = {
-  title: "Chatbook",
-  description: "Share your AI Adventures with the World!",
+  title: "ChatBook",
+  description: "Share your AI generated content with the world. This is a social media platform and global marketplace for AI generated content.",
 };
 
 export default function RootLayout({
@@ -20,9 +22,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={cn("font-IBMPlex antialised", IBMPlex.variable )}>{children}</body>
-      {children }
-    </html>
+    <ClerkProvider appearance={{
+      variables: { colorPrimary: '#624cf5' }
+    }}>
+      <html lang="en">
+        <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
