852e5a36bf6799c3c729e1e0a3fb6b701beabc5d
Lukas-Santo-Puglisi <lsanpuglisi@gmail.com>
2024-02-25 18:54:19 +0100

    bug in upload presets fixed

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -23,7 +23,7 @@ export const connectToDatabase = async () => {
   cached.promise = 
     cached.promise || 
     mongoose.connect(MONGODB_URL, { 
-      dbName: 'imaginify', bufferCommands: false 
+      dbName: 'chatbook', bufferCommands: false 
     })
 
   cached.conn = await cached.promise;
