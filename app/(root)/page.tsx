ff015444f98bef810e5af85dba34cd93535e1701
Lukas-Santo-Puglisi <lsanpuglisi@gmail.com>
2024-03-02 14:48:35 +0100

    transformed image download

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,8 +2,13 @@ import { navLinks } from '@/constants'
 import Link from 'next/link'
 import React from 'react'
 import Image from 'next/image'
-
-const Home = () => {
+import { Collection } from '@/components/shared/Collection'
+import { getAllImages } from '@/lib/actions/image.actions'
+const Home = async ({searchParams}: SearchParamProps) => {
+  
+  const page = Number(searchParams?.page) || 1;
+  const searchQuery = (searchParams?.query as string) || "";
+  const images = await getAllImages({page, searchQuery}); 
   return (
     <>
       <section className="home">
@@ -33,6 +38,16 @@ const Home = () => {
         ))}
       </ul>
       </section>
+      <section className="sm:mt-12 ">
+        < Collection 
+          hasSearch={true}
+          images={images?.data} 
+          totalPages={images?.totalPages}
+          page={page}
+
+        
+        />
+      </section> 
     </>
   )
 }
